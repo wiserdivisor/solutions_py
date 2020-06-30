8c47286e9edb18a55908dddc77972f3a03bdfd38
wiserdivisor <dat0user@gmail.com>
2020-07-01 02:30:14 +0530

    commit_3

---
+++ HackerEarth/Practice/jadoo_and_dna.py
@@ -0,0 +1,2 @@
+try:print(''.join(["CGAU"["GCTA".index(c)]for c in input()]))
+except:print("Invalid Input")
--- Random/try.py
+++ Random/try.py
@@ -1,1 +1,14 @@
-##Blank;
+s = input()
+out = ""
+flag = True
+for i in s:
+    if  (i=='G'):out+='C'
+    elif(i=='C'):out+='G'
+    elif(i=='A'):out+='U'
+    elif(i=='U'):out+='A'
+    elif(i=='T'):out+='A'
+    else:
+        flag = False
+        print("Invalid Input")
+        break
+print(out*flag)
